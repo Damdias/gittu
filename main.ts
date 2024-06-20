c09908b8e4ec984bbda111f4314b4e02bffcb5d1
Damith <damithasanka@gmail.com>
2024-06-20 11:05:57 +0530

    add address

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 console.log("hi Main");
 
 function display(name: string, address: string) {
-  console.log(`Company Name ${name}, `);
+  console.log(`Company Name ${name},${address}`);
 }
 
 function add(val: number, val2: number) {
